ba3d8b728bc7c1ac89ee97ce038b3b04e2d35e05
binghesam <bing_he@foxmail.com>
2017-07-11 10:05:13 +0800

    Update city0Twitter.sh

--- city0Twitter.sh
+++ city0Twitter.sh
@@ -1,5 +1,5 @@
 # /usr/bin/sh
 cd ..
 javac -d bin -sourcepath src -cp lib/twitter4j-core-4.0.4.jar:lib/twitter4j-stream-4.0.4.jar src/MultiThreadLocationAdvancedFillStreamFilterLinux00.java
-java -Xmx1900M -cp bin:lib/twitter4j-core-4.0.4.jar:lib/twitter4j-stream-4.0.4.jar twitter4JBing.MultiThreadLocationAdvancedFillStreamFilterLinux00
+java -Xmx1800M -cp bin:lib/twitter4j-core-4.0.4.jar:lib/twitter4j-stream-4.0.4.jar twitter4JBing.MultiThreadLocationAdvancedFillStreamFilterLinux00
 #java -Xmx5120M -cp bin:lib/twitter4j-core-4.0.4.jar:lib/twitter4j-stream-4.0.4.jar twitter4JBing.MultiThreadLocationAdvancedFillStreamFilterLinux00
