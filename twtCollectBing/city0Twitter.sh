93ad41022edf5db55c1dbc9cf25947ffa31a98df
binghesam <bing_he@foxmail.com>
2019-07-20 06:40:13 +0800

    Add files via upload

---
+++ twtCollectBing/city0Twitter.sh
@@ -0,0 +1,4 @@
+# /usr/bin/sh
+cd ..
+javac -d bin -sourcepath src -cp lib/twitter4j-core-4.0.4.jar:lib/twitter4j-stream-4.0.4.jar src/MultiThreadLocationAdvancedFillStreamFilterLinux00.java
+java -Xmx1800M -cp bin:lib/twitter4j-core-4.0.4.jar:lib/twitter4j-stream-4.0.4.jar twitter4JBing.MultiThreadLocationAdvancedFillStreamFilterLinux00
---
+++ twtCollectBing/printTest.java
@@ -0,0 +1,5 @@
+public class printTest{
+	public static void main(String[] args){
+		System.out.println("test for something");	
+	}
+}
